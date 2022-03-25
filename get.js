ca4049ef12413f94fb9c39d18d7e77435ddaf088
Met <memetrusidovski@gmail.com>
2022-03-25 14:38:51 -0400

    deploy

---
+++ get.js
@@ -0,0 +1,16 @@
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+// A `main` function so that you can use async/await
+async function mainPlace() {
+  const user = await prisma.place.findMany();
+
+    console.log(user);
+}
+
+
+
+module.exports = {
+  mainPlace: mainPlace
+};
